39d8e7cb7c41a43879733022dfc9670497f77d41
Christopher Nsikak <christophernsikak1@gmail.com>
2019-09-28 20:11:40 +0100

    website with basic username and password setup

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,5 @@
 from app import app
-from flask import render_template, url_for
-
+from flask import render_template, request
 
 @app.route('/index')
 def hello():
@@ -15,3 +14,9 @@ def index():
 @app.route('/login')
 def login():
     return render_template("login.html")
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    return "Your Username is: {} and the Password is: {}".format(username, password)
